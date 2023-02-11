0a770fb831914286c0297b3c3bb64514200f6a67
karthini <39013253+karthini@users.noreply.github.com>
2023-02-11 15:26:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit;
 streamlit.title('My parents new healthy dinner')
-streamlit.header('Breakfast Menu)
+streamlit.header('Breakfast Menu')
 streamlit.text('Banana oatmeal')
 streamlit.text('Kale egg and avacado')
 streamlit.text('Egg omelette with mushroom')
