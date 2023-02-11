a29612f0c3061312143dedbb0c2c4727ab18c04b
karthini <39013253+karthini@users.noreply.github.com>
2023-02-11 16:57:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
+import streamlit;
+streamlit.title(' My parents new healthy dinner')
+streamlit.header('🙌Breakfast Menu')
+streamlit.text('🍌🥝Banana oatmeal')
+streamlit.text('🥪🥪Kale egg and avacado')
+streamlit.text('🍳🍟🍵Egg omelette with mushroom')
+
+streamlit.header('Build your own fruit smoothie')
+
 import pandas;
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
