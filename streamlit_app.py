c24d2c831b5ebf970ee95bfdc6f496b47117490d
karthini <39013253+karthini@users.noreply.github.com>
2023-02-12 13:22:50 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,6 @@ streamlit.header('Build your own fruit smoothie')
 
 import pandas;
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.header('Build your own fruit smoothie')
+streamlit.header('Build your own .m fruit smoothie')
 streamlit.dataframe(my_fruit_list)
 
