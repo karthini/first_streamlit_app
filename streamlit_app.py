59fd39e44f412a7a63700d247335c6f423a126ce
karthini <39013253+karthini@users.noreply.github.com>
2023-02-16 08:48:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,6 @@ streamlit.header('Build your own .m fruit smoothie')
 realans = ['', 'abc', 'edf']
 streamlit.dataframe(my_fruit_list)
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
